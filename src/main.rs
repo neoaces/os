08b901a7e9d980f526bfe2220215d8f713a908ea
Anzle Chavez <anzlemchavez@gmail.com>
2023-07-18 22:39:11 -0400

    added a static BUFFER and support for write! macro

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@
 
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
+// static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // Prevents the compiler from changing the name of _start()
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_ex("HEllo world");
-    
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello!").unwrap();
+
+    // With write! macro for more type support
+    write!(vga_buffer::WRITER.lock(), "{}", 7).unwrap();
     loop {}
 }
 
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,16 @@
+use volatile::Volatile;
+use core::fmt;
+use lazy_static::lazy_static;
+use spin::Mutex;
 static VGA_TEXT_BUFFER: isize = 0xB8000;
+lazy_static! {
+	pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer { 
+		// Ref needed for lazy_static
+		column_position: 0,
+		color_code: ColorCode::new(Color::Yellow, Color::Black),
+		buffer: unsafe {&mut *(VGA_TEXT_BUFFER as *mut Buffer)}
+	});
+}
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -52,7 +64,7 @@ const BUFFER_WIDTH: usize = 80;
 struct Buffer {
 	// Creates an array of ScreenChars with size BUFFER_WIDTH x BUFFER_HEIGHT
 	// Represents the entire screen
-	chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT]
+	chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT]
 	// [[T; #cols]; #rows]
 }
 
@@ -63,8 +75,6 @@ pub struct Writer {
 }
 
 impl Writer {
-	fn new_line(&mut self) {} // Requires a mutable borrow to itself
-
 	pub fn write_byte(&mut self, byte: u8) {
 		match byte {
 			b'\n' => self.new_line(),
@@ -74,15 +84,15 @@ impl Writer {
 					// Must move to next line when at the border
 					self.new_line();
 				}
-
-				let row = BUFFER_HEIGHT - 1; // TODO: Why is it deincremented?
+				// Prints from the top left, thus to print at the bottom, needs to decrement 1 using top of row to print char under
+				let row = BUFFER_HEIGHT - 1; 
 				let col = self.column_position;
 				
 				let color_code = self.color_code; // Copies the color_code
-				self.buffer.chars[row][col] = ScreenChar {
+				self.buffer.chars[row][col].write(ScreenChar {
 					ascii_character: byte,
 					color_code,
-				};
+				});
 
 				self.column_position += 1; // Increments the column forward
 			}
@@ -99,29 +109,61 @@ impl Writer {
 			}
 		}
 	}
+
+	fn new_line(&mut self) {
+		for row in 1..BUFFER_HEIGHT {
+			for col in 0..BUFFER_WIDTH {
+				let character = self.buffer.chars[row][col].read(); // Looks at the char at nth position
+				self.buffer.chars[row-1][col].write(character)
+			}
+		}
+
+		self.clear_row(BUFFER_HEIGHT - 1);
+		self.column_position = 0;
+	}
+
+	fn clear_row(&mut self, row: usize) {
+		let blank = ScreenChar {
+			ascii_character: b' ', // Replaces the row with spaces
+			color_code: self.color_code,
+		};
+
+		for col in 0..BUFFER_WIDTH {
+			self.buffer.chars[row][col].write(blank);
+		}
+	}
 }
 
-// Example case:
-pub fn print_ex(string: &str) {
-	let mut writer = Writer {
-		column_position: 0,
-		color_code: ColorCode::new(Color::Blue, Color::Black),
-		// A pointer to the memory address of the VGA_BUFFER
-		buffer: unsafe {&mut *(VGA_TEXT_BUFFER as *mut Buffer)}
-
-	};
-
-	writer.write_string(string)
+impl fmt::Write for Writer { // Allows printing integers and floats
+	// Implements the Write trait
+	fn write_str(&mut self, s: &str) -> fmt::Result {
+		self.write_string(s);
+		Ok(())
+	}
 }
 
-pub fn print_ex_char(chars: &char) {
-	let mut writer = Writer {
-		column_position: 0,
-		color_code: ColorCode::new(Color::Blue, Color::Black),
-		// A pointer to the memory address of the VGA_BUFFER
-		buffer: unsafe {&mut *(VGA_TEXT_BUFFER as *mut Buffer)}
+// Example case: NOT REQUIRED AFTER WRITER STATIC
+// pub fn print_ex(string: &str) {
+// 	use core::fmt::Write;
+// 	let mut writer = Writer {
+// 		column_position: 0,
+// 		color_code: ColorCode::new(Color::LightRed, Color::Black),
+// 		// A pointer to the memory address of the VGA_BUFFER
+// 		buffer: unsafe {&mut *(VGA_TEXT_BUFFER as *mut Buffer)}
 
-	};
+// 	};
 
-	writer.write_byte(*chars as u8); // Since ascii_character is in u8
-}
+// 	writeln!(writer, "{}", string).unwrap();
+// }
+
+// pub fn print_ex_char(chars: &char) {
+// 	let mut writer = Writer {
+// 		column_position: 0,
+// 		color_code: ColorCode::new(Color::Blue, Color::Black),
+// 		// A pointer to the memory address of the VGA_BUFFER
+// 		buffer: unsafe {&mut *(VGA_TEXT_BUFFER as *mut Buffer)}
+
+// 	};
+
+// 	writer.write_byte(*chars as u8); // Since ascii_character is in u8
+// }
